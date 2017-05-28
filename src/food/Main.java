28b4aa0bcdec3c1b140bc4f9256e988a4a0283fc
cirolas <cirolas@DESKTOP-IM7F3I0>
2017-05-28 22:49:21 +0200

    Cambios en el segundo Commit

--- src/food/Main.java
+++ src/food/Main.java
@@ -1,24 +1,39 @@
 package food;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.Random;
 
 
-
+/**
+ * 
+ * @author David Ordóñez Valiña
+ */
 class Main {
  
-     public static void main(String[] args) throws IOException {
-         try (FileWriter fichero = new FileWriter("ListadoPeliculas.txt")) {
-             
-             for (int i = 0; i < 10; i++) {
-                 fichero.write("Hola ");
-                 
-             }
-         } 
-         catch (Exception e) {
-             System.out.println(e.getMessage());
-         }
+     public static void main(String[] args) {
+      
+         Random rd = new Random();
+         int a;
+       
+        int [] numeros = new int [2];
+        int [] todos = new int [12];
          
+         for (int i = 0; i < numeros.length; i++) {
+             a= rd.nextInt(12)+1;
+             if (todos[a-1]==0) {
+                 numeros[i]=a;
+             }
+             else {
+                 a= rd.nextInt(50)+1;
+             if (todos[a-1]==0) {
+                 numeros[i]=a;
+             }
+             }
+                 
+             
+             
+         }
+         System.out.println(Arrays.toString(numeros));
     }
   
              
